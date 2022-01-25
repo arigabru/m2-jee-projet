15550828ee84165c66995cc4ad58f433024a9a67
Bruno Ariganello <brunari@live.fr>
2022-01-25 21:06:42 +0100

    Ajout de CrossOrigin

--- back/src/main/java/univ/m2/jee_2021_2022/controller/BatailleController.java
+++ back/src/main/java/univ/m2/jee_2021_2022/controller/BatailleController.java
@@ -2,7 +2,9 @@ package univ.m2.jee_2021_2022.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,23 +13,21 @@ import univ.m2.jee_2021_2022.model.bataille.Couleur;
 import univ.m2.jee_2021_2022.model.bataille.Valeur;
 import univ.m2.jee_2021_2022.service.CarteService;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
+@RequestMapping("/bataille")
 public class BatailleController {
 
     @Autowired
     private CarteService carteService;
 
-    @GetMapping("/bataille/paquet/carteRand")
-<<<<<<< HEAD
+    @GetMapping("/carteRand")
     public ResponseEntity<Carte> getCarteAleatoire() {
-=======
-    public ResponseEntity<Carte> getCarteAleatoire(@RequestParam(value = "id") int id) {
->>>>>>> 3d407b67efd537701e1bba8351f6e0fe84976699
         // À finir quand la base sera faite
         return ResponseEntity.ok(new Carte(Valeur.AS, Couleur.PIQUE));
     }
 
-    @GetMapping("/bataille/comparer{c1}{c2}")
+    @GetMapping("/comparer{c1}{c2}")
     public ResponseEntity<Integer> getComparaison(@RequestParam(value = "c1") Carte c1,
             @RequestParam(value = "c2") Carte c2) {
         return ResponseEntity.ok(carteService.comparerCarte(c1, c2));
